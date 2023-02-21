0d7f4eef7118b939693a7cc5abdb2b61de0b5c3e
Andreu Nuwe <andreu@nuwe.io>
2023-02-21 18:00:47 +0100

    increase coverage

--- src/main/java/com/nuwe/App.java
+++ src/main/java/com/nuwe/App.java
@@ -2,13 +2,6 @@ package com.nuwe;
 
 public class App {
 
-    public static void main(String[] args) {
-        int a = sum2(1, 2);
-        int b = sum2(3, 4);
-        int c = sum2(5, 6);
-        System.out.println(a + b + c);
-    }
-
     public static int sum2(int a, int b) {
         return a + b;
     }
