e548baedc838deeb7f443f728b7b9a1400fa5ef6
Andreu Nuwe <andreu@nuwe.io>
2023-02-21 16:30:01 +0100

    Simpler

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,9 @@
 public class App {
 
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
     public static int sum2(int a, int b) {
         return a + b;
     }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -5,11 +5,11 @@ public class AppTest {
 
   @Test
   public void myPassedTest() {
-      assertEquals(App.sum2(3, 4), 7);
+      assertEquals(7, App.sum2(3, 4));
   }
   
   @Test
   public void myFailedTest() {
-    assertEquals(App.sum2(3, 9), 9);
+    assertEquals(9, App.sum2(3, 9));
   }
 }
