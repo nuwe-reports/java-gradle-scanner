7500c5cc1d0c35aaac3c1ed5f62d69c0fcebd13b
Andreu Nuwe <andreu@nuwe.io>
2023-02-21 17:58:40 +0100

    increase coverage

--- src/main/java/com/nuwe/App.java
+++ src/main/java/com/nuwe/App.java
@@ -7,21 +7,6 @@ public class App {
         int b = sum2(3, 4);
         int c = sum2(5, 6);
         System.out.println(a + b + c);
-
-        int d = sum2(7, 8);
-        int e = sum2(9, 10);
-        int f = sum2(11, 12);
-        System.out.println(d + e + f);
-
-        int g = sum2(13, 14);
-        int h = sum2(15, 16);
-        int i = sum2(17, 18);
-        System.out.println(g + h + i);
-
-        int j = sum2(19, 20);
-        int k = sum2(21, 22);
-        int l = sum2(23, 24);
-        System.out.println(j + k + l);
     }
 
     public static int sum2(int a, int b) {
